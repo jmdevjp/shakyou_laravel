0494aa127332a8c440da7b4b9bda4a303e9ab1d3
jmdevjp <88482250+jmdevjp@users.noreply.github.com>
2021-07-08 10:32:09 +0900

    ユーザー登録時のフォームリクエストを追加

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,7 @@ Route::post('/login', [LoginController::class, 'authenticate'])
 Route::get('/logout', [LoginController::class, 'logout'])
     ->middleware('auth')
     ->name('logout');
+
+use App\Http\Controllers\UserController;
+
+Route::get('/user/register', [UserController::class, 'register']);
