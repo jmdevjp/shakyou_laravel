9a668c1086926f92fa8561d4235db56fea710859
jmdevjp <88482250+jmdevjp@users.noreply.github.com>
2021-07-10 18:43:04 +0900

    Authorのモデルを追加

---
+++ app/Models/Author.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+
+    // // 別名で関連付けることができる。
+    // protected $table = 't_author';
+
+    // // 主キーを id でなく author_id にする。
+    // protected $primaryKey = 'author_id';
+
+    // // タイムスタンプを記録しない。
+    // protected $timestamp = false;
+
+    //
+    // Mass assignmentに対する脆弱性対策
+    //
+    // $fillable と $guarded は排他使用。
+    //
+    // // 編集可能なカラムを指定する(ホワイトリスト方式)
+    // protected $fillable = [
+    //     'name',
+    //     'kana',
+    // ];
+    // // 編集を認めないカラムを指定する(ブラックリスト方式)
+    // protected $guarded = [
+    //     'id',
+    //     'created_at',
+    //     'updated_at',
+    // ];
+}
