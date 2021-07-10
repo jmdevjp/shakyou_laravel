d296791cd54256da4607de54cad27c90d1f61476
jmdevjp <88482250+jmdevjp@users.noreply.github.com>
2021-07-10 18:43:22 +0900

    Authorの論理削除に対応

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Author extends Model
 {
@@ -34,4 +35,6 @@ class Author extends Model
     //     'created_at',
     //     'updated_at',
     // ];
+
+    use SoftDeletes;
 }
